3fed071877f9ae22723262682dc2e8a49ccedc00
101Thapelo <50619622+101Thapelo@users.noreply.github.com>
2019-07-30 14:30:01 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import{RouterModule, Routes} from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'',component:SignInComponent},
+      {path:'sign',component:SignInComponent},
+      {path:'home',component:HomeComponent},
+  ])],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
+
+
 export class AppModule { }
